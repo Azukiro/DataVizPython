aa4b761850e49e8beac850e7d6859f934ebba4c7
Azukiro <46447981+Azukiro@users.noreply.github.com>
2020-09-25 15:02:34 +0200

    ee

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 
 import pandas as pd
 from consts import *
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
 
-# 47 317
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-if __name__ == "__main__":
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-    df = pd.read_csv(CSV_DATA_PATH, sep=';', low_memory=False, encoding="UTF-8", usecols=CSV_DATA_HEADERS)
-    print(df)
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_csv(CSV_DATA_PATH, sep=';', low_memory=False, encoding="UTF-8", usecols=CSV_DATA_HEADERS)
+
+fig = fig = px.histogram(df, x="puiss_max",  nbins=1000)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
